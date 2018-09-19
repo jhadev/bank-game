987aaf4b8b71094f1089bf6b86b017d4a7674c5b
JA <joshappeldev@gmail.com>
2018-09-19 16:47:38 -0400

    started pseudocode

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,17 @@
+//set up variables for game
+var winNum = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+
+var walt = Math.floor(Math.random() * 12) + 1;
+var pinkman = Math.floor(Math.random() * 12) + 1;
+var gus = Math.floor(Math.random() * 12) + 1;
+var saul = Math.floor(Math.random() * 12) + 1;
+var score = 0;
+//create function to randomly choose winning number
+
+//create functions to randomly select value for each image
+
+//create click events for each image
+
